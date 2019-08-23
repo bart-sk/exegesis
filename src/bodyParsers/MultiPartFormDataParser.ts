c63b4215a0bbbbbd16408f1a7fe38089461f8c48
Ladislav Perduk <perduk@bart.sk>
2019-08-23 09:18:04 +0200

    Edited support for multipart

--- src/bodyParsers/MultiPartFormDataParser.ts
+++ src/bodyParsers/MultiPartFormDataParser.ts
@@ -13,7 +13,7 @@ export default class MultiPartFormDataParser implements MimeTypeParser {
 
     parseReq(req: http.IncomingMessage, _res: http.ServerResponse, done: Callback<void>) : void {
         this._bodyParserMiddlware.parse(req, (_err: any, _fields: any, files: any) => {
-            done(null, files);
+            done(_err, files);
         });
     }
 }
