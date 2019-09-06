a18c471d75aef1d7dce599c10249b898c4c38c3c
Samuel Sepesi <sepesi@bart.sk>
2019-09-06 13:34:46 +0200

    Change file size limit for upload

--- src/bodyParsers/MultiPartFormDataParser.ts
+++ src/bodyParsers/MultiPartFormDataParser.ts
@@ -1,17 +1,17 @@
 import http from 'http';
-import {IncomingForm} from 'formidable';
+import { IncomingForm } from 'formidable';
 
-import {Callback, BodyParser} from "../types";
+import { Callback, BodyParser } from '../types';
 
 export default class MultiPartFormDataParser implements BodyParser {
-    private _bodyParserMiddlware : any;
+    private _bodyParserMiddlware: any;
 
-    constructor(
-    ) {
+    constructor() {
         this._bodyParserMiddlware = new IncomingForm();
+        this._bodyParserMiddlware.maxFieldsSize = 2000 * 1024 * 1024;
     }
 
-    parseReq(req: http.IncomingMessage, _res: http.ServerResponse, done: Callback<void>) : void {
+    parseReq(req: http.IncomingMessage, _res: http.ServerResponse, done: Callback<void>): void {
         this._bodyParserMiddlware.parse(req, (_err: any, _fields: any, files: any) => {
             done(_err, files);
         });
