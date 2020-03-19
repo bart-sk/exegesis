ce07e8cf3e464575df9fd88d4a983fbd4389ed53
Lukáš Grochoľ <grochol@MacBook-Pro-uzivatela-Lukas.local>
2020-03-19 10:53:07 +0100

    Vytvarania vzdy novej instancie formidable pri uploade

--- src/bodyParsers/MultiPartFormDataParser.ts
+++ src/bodyParsers/MultiPartFormDataParser.ts
@@ -1,19 +1,16 @@
+import { IncomingForm } from 'formidable';
 import http from 'http';
-import {IncomingForm} from 'formidable';
+import { BodyParser, Callback } from "../types";
 
-import {Callback, BodyParser} from "../types";
 
 export default class MultiPartFormDataParser implements BodyParser {
-    private _bodyParserMiddlware : any;
-
-    constructor(
-    ) {
-        this._bodyParserMiddlware = new IncomingForm();
-    }
-
-    parseReq(req: http.IncomingMessage, _res: http.ServerResponse, done: Callback<void>) : void {
-        this._bodyParserMiddlware.parse(req, (_err: any, _fields: any, files: any) => {
-            done(_err, files);
+    parseReq(req: http.IncomingMessage, _res: http.ServerResponse, done: Callback<void>): void {
+        const parser = new IncomingForm();
+        parser.parse(req, (_err: any, _fields: any, files: any) => {
+            if (_err) {
+                return done(_err);
+            }
+            return done(null, files);
         });
     }
 }
