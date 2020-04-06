bb313a42b94bfb910b9077185565315753252e16
Vladimir Darak <darak@bart.sk>
2020-04-06 12:39:44 +0200

    Multipart form data - doplnena moznost nastavit maxFileSize a uploadDir pre MultipartFormDataParser

--- src/bodyParsers/MultiPartFormDataParser.ts
+++ src/bodyParsers/MultiPartFormDataParser.ts
@@ -3,8 +3,21 @@ import http from 'http';
 import { BodyParser, Callback } from '../types';
 
 export default class MultiPartFormDataParser implements BodyParser {
+    private _options: MultipartFormDataOptions;
+
+    constructor(options: MultipartFormDataOptions) {
+        // FIXME: https://github.com/expressjs/body-parser/issues/304
+        this._options = options;
+    }
+
     parseReq(req: http.IncomingMessage, _res: http.ServerResponse, done: Callback<void>): void {
         const parser = new IncomingForm();
+        if (this._options.maxFileSize) {
+            parser.maxFileSize = this._options.maxFileSize;
+        }
+        if (this._options.uploadDir) {
+            parser.uploadDir = this._options.uploadDir;
+        }
         parser.parse(req, (_err: any, _fields: any, files: any) => {
             if (_err) {
                 return done(_err);
@@ -13,3 +26,8 @@ export default class MultiPartFormDataParser implements BodyParser {
         });
     }
 }
+
+type MultipartFormDataOptions = {
+    uploadDir?: string;
+    maxFileSize?: number;
+};
